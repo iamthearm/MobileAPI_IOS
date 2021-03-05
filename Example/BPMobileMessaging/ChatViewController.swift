1d220bfd23acfe3526c6b4d2b1bc211a3b7b99bc
Dmitry Rybakov <external.dmytro.rybakov@se.bosch.com>
2021-03-05 11:19:07 +0100

    Integrate MessageKit example

--- Example/BPMobileMessaging/ChatViewController.swift
+++ Example/BPMobileMessaging/ChatViewController.swift
@@ -1,11 +1,16 @@
 //
 // Copyright © 2021 BrightPattern. All rights reserved. 
-    
 
-import Foundation
 import UIKit
+import MessageKit
+import InputBarAccessoryView
 
-class ChatViewController: ViewController, ServiceDependencyProviding {
+extension UIColor {
+    static let primaryColor = UIColor(red: 69/255, green: 193/255, blue: 89/255, alpha: 1)
+}
+
+/// A base class for the example controllers
+class ChatViewController: MessagesViewController, MessagesDataSource, ServiceDependencyProviding {
     var service: ServiceDependencyProtocol?
     var currentChatID: String?
     lazy var viewModel: ChatViewModel = {
@@ -17,49 +22,199 @@ class ChatViewController: ViewController, ServiceDependencyProviding {
         return model
     }()
 
-    @IBOutlet weak var chatTableView: UITableView!
+    // MARK: - Private properties
+
+    private let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        chatTableView.register(ChatBubbleCell.self, forCellReuseIdentifier: ChatBubbleCell.reuseIdentifier)
-    }
-}
 
-extension ChatViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        viewModel.sectionsCount
+        configureMessageCollectionView()
+        configureMessageInputBar()
+
+        viewModel.delegate = self
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        viewModel.numberOfRows(section: section)
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatBubbleCell.reuseIdentifier, for: indexPath) as? ChatBubbleCell else {
+    func configureMessageCollectionView() {
 
-            return UITableViewCell()
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        scrollsToBottomOnKeyboardBeginsEditing = true
+        maintainPositionOnKeyboardFrameChanged = true
+    }
+
+    func configureMessageInputBar() {
+        messageInputBar.delegate = self
+        messageInputBar.inputTextView.tintColor = .primaryColor
+        messageInputBar.sendButton.setTitleColor(.primaryColor, for: .normal)
+        messageInputBar.sendButton.setTitleColor(
+            UIColor.primaryColor.withAlphaComponent(0.3),
+            for: .highlighted
+        )
+    }
+
+    // MARK: - Helpers
+
+    func isLastSectionVisible() -> Bool {
+        guard let lastIndexPath = viewModel.lastMessageIndexPath else {
+            return false
         }
-        let message = viewModel.chatMessage(at: indexPath.row)
-        configure(cell, at: indexPath, with: message)
-        return cell
+        return messagesCollectionView.indexPathsForVisibleItems.contains(lastIndexPath)
     }
 
-    private func configure(_ cell: ChatBubbleCell, at: IndexPath, with message: ChatMessage) {
-        cell.bubbleImage = #imageLiteral(resourceName: "bubbleMine")
-        cell.message = message
+    // MARK: - MessagesDataSource
+
+    func currentSender() -> SenderType {
+        viewModel.currentSender
+    }
+
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        viewModel.chatMessagesCount()
+    }
+
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        viewModel.chatMessage(at: indexPath.section)
+    }
+
+    func cellTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        if indexPath.section % 3 == 0 {
+            return NSAttributedString(string: MessageKitDateFormatter.shared.string(from: message.sentDate), attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 10), NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+        }
+        return nil
+    }
+
+    func cellBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        NSAttributedString(string: "Read", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 10), NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+    }
+
+    func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        let name = message.sender.displayName
+        return NSAttributedString(string: name, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption1)])
+    }
+
+    func messageBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        let dateString = formatter.string(from: message.sentDate)
+        return NSAttributedString(string: dateString, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption2)])
     }
 }
 
-extension ChatViewController: UITableViewDelegate {
+// MARK: - MessagesDisplayDelegate
 
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        viewModel.heightForRow(at: indexPath)
+extension ChatViewController: MessagesDisplayDelegate {
+
+    // MARK: - Text Messages
+
+    func textColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+        return isFromCurrentSender(message: message) ? .white : .darkText
+    }
+
+    func detectorAttributes(for detector: DetectorType, and message: MessageType, at indexPath: IndexPath) -> [NSAttributedString.Key: Any] {
+        switch detector {
+        case .hashtag, .mention: return [.foregroundColor: UIColor.blue]
+        default: return MessageLabel.defaultAttributes
+        }
+    }
+
+    func enabledDetectors(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> [DetectorType] {
+        return [.url, .address, .phoneNumber, .date, .transitInformation, .mention, .hashtag]
+    }
+
+    // MARK: - All Messages
+
+    func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+        return isFromCurrentSender(message: message) ? .primaryColor : UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
+    }
+
+    func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle {
+
+        let tail: MessageStyle.TailCorner = isFromCurrentSender(message: message) ? .bottomRight : .bottomLeft
+        return .bubbleTail(tail, .curved)
+    }
+}
+
+// MARK: - MessagesLayoutDelegate
+
+extension ChatViewController: MessagesLayoutDelegate {
+
+    func cellTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 18
+    }
+
+    func cellBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 17
+    }
+
+    func messageTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 20
+    }
+
+    func messageBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 16
+    }
+
+}
+
+// MARK: - MessageInputBarDelegate
+
+extension ChatViewController: InputBarAccessoryViewDelegate {
+
+    @objc
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        processInputBar(messageInputBar)
+    }
+
+    func processInputBar(_ inputBar: InputBarAccessoryView) {
+        // Here we can parse for which substrings were autocompleted
+        let attributedText = inputBar.inputTextView.attributedText!
+        let range = NSRange(location: 0, length: attributedText.length)
+        attributedText.enumerateAttribute(.autocompleted, in: range, options: []) { (_, range, _) in
+
+            let substring = attributedText.attributedSubstring(from: range)
+            let context = substring.attribute(.autocompletedContext, at: 0, effectiveRange: nil)
+            print("Autocompleted: `", substring, "` with context: ", context ?? [])
+        }
+
+        let components = inputBar.inputTextView.components
+        inputBar.inputTextView.text = String()
+        inputBar.invalidatePlugins()
+        // Send button activity animation
+        inputBar.sendButton.startAnimating()
+        inputBar.inputTextView.placeholder = "Sending..."
+        // Resign first responder for iPad split view
+        inputBar.inputTextView.resignFirstResponder()
+        viewModel.userEnteredData(components) {
+            inputBar.sendButton.stopAnimating()
+            inputBar.inputTextView.placeholder = "Aa"
+        }
     }
 }
 
 extension ChatViewController: ChatViewModelUpdatable {
     func update() {
-        // TODO: optimize it
-        chatTableView.reloadData()
+        guard viewModel.chatMessagesCount() > 0 else {
+            return
+        }
+        // Reload last section to update header/footer labels and insert a new one
+        messagesCollectionView.performBatchUpdates({
+            messagesCollectionView.insertSections([viewModel.chatMessagesCount() - 1])
+            if viewModel.chatMessagesCount() >= 2 {
+                messagesCollectionView.reloadSections([viewModel.chatMessagesCount() - 2])
+            }
+        }, completion: { [weak self] _ in
+            if self?.isLastSectionVisible() == true {
+                self?.messagesCollectionView.scrollToBottom(animated: true)
+            }
+        })
     }
 }
