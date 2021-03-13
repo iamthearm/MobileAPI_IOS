b9d15bf1a3e8ecd410deb17e6051c52a9176c789
Alexander Lobastov <58867146+ALobastov@users.noreply.github.com>
2021-03-12 18:22:38 -0800

    Clean up the docs; make some structs/classes private

--- BPMobileMessaging/Classes/ConactCenterCommunication/Model/ContactCenterServiceAvailability.swift
+++ BPMobileMessaging/Classes/ConactCenterCommunication/Model/ContactCenterServiceAvailability.swift
@@ -7,12 +7,15 @@ import Foundation
 /// Describes chat service availability states.
 /// - Tag: ContactCenterServiceChatAvailability
 public enum ContactCenterServiceChatAvailability: String, Decodable {
+    /// Chat service is available; application may request new chat sessions
     case available
+    /// Chat service unavailable; application should not request new chat sessions
     case unavailable
 }
 
 /// Describes chat service status.
 /// - Tag: ContactCenterServiceAvailability
 public struct ContactCenterServiceAvailability: Decodable {
+    /// Current chat service availability state.
     public let chat: ContactCenterServiceChatAvailability
 }
