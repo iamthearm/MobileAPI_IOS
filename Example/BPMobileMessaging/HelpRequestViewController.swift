641ad218447ee703daadd53532cd42b5ccfc551f
Alexander Lobastov <58867146+ALobastov@users.noreply.github.com>
2021-03-31 19:20:57 -0700

    Fix crash on request chat on physical iPhone device

--- Example/BPMobileMessaging/HelpRequestViewController.swift
+++ Example/BPMobileMessaging/HelpRequestViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class HelpRequestViewController: ViewController, ServiceDependencyProviding {
     var service: ServiceDependencyProtocol?
     var bundleIdentifier: String = Bundle.main.bundleIdentifier ?? ""
-    @IBOutlet weak var pastConversationsBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var problemDescription: UITextView!
     @IBOutlet weak var helpMeButton: UIButton!
     @IBOutlet weak var caseNumber: UITextField!
@@ -89,9 +88,7 @@ class HelpRequestViewController: ViewController, ServiceDependencyProviding {
         // get a rect for the textView frame
         let containerFrame = problemDescription.frame
         let diff = keyboardBounds.origin.y - containerFrame.maxY
-        if diff < 10 {
-            self.pastConversationsBottomConstraint.constant += 10 - diff
-        }
+
         UIView.beginAnimations(nil, context: nil)
         UIView.setAnimationBeginsFromCurrentState(true)
         UIView.setAnimationDuration(duration.doubleValue)
@@ -101,7 +98,6 @@ class HelpRequestViewController: ViewController, ServiceDependencyProviding {
     }
 
     private func resetBottomSpace() {
-//        pastConversationsBottomConstraint.constant = viewModel.bottomSpace
     }
 
     @objc
