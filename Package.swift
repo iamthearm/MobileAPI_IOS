97b34e187c345424dd8501bc5fcff4e19967e575
Anthony <anthonyrmiles@gmail.com>
2023-08-29 14:25:27 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -12,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BrightPattern",
-            targets: ["BrightPattern"])
+            targets: ["BrightPattern"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
