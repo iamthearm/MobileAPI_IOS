4a06fff33ab00cd3c798423450f0ae1a74c98851
Anthony Miles <anthony.miles@velocitor.com>
2023-08-29 14:31:00 -0400

    updated package def

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -22,10 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BrightPattern",
+            dependencies: [],
             path: "BPMobileMessaging/Classes/BrightPattern"),
-            dependencies: []),
-        .testTarget(
-            name: "BrightPatternTests",
-            dependencies: ["BrightPattern"]),
     ]
 )
+
