243b0860cc5fd8236b2e79a1a9738277faf20e09
Anthony <anthonyrmiles@gmail.com>
2023-08-29 14:21:04 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BrightPattern",
-            targets: ["BrightPattern"]),
+            targets: ["BrightPattern"],
+            path: "BPMobileMessaging/Classes/BrightPattern"),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
